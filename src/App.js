b86175fd129968595e1d66f3da675100c7b24459
day1park <daypark2408@gmail.com>
2019-06-22 18:28:54 +1000

    create hero component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Navbar from "./components/layout/Navbar";
+import Hero from "./components/layout/Hero";
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
