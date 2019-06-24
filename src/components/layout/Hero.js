f4f80ec428b132b44178ddd906714da0a639b1b8
day1park <daypark2408@gmail.com>
2019-06-24 23:31:32 +1000

    landing page text

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -5,6 +5,9 @@ export default class Hero extends Component {
     return (
       <div>
         <h1>HERO</h1>
+        <h1>Hi I'm day park</h1>
+        <p>a full stack web developer</p>
+        <a href="#button">Read More</a>
       </div>
     );
   }
